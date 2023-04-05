a690afff6078c03a0f4629b284b9129af5b869ad
orumurin <orumurin@gmail.com>
2023-04-05 12:22:44 +0300

    add base DB structure

---
+++ app/Enums/PaymentTypesEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum PaymentTypesEnum
+{
+    case Income;
+    case Expense;
+}
---
+++ database/migrations/2023_04_05_085620_create_incomes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('value')->default(0);
+            $table->text('description')->nullable();
+            $table->date('date')->default(now());
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incomes');
+    }
+};
