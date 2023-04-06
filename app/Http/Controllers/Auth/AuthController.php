44e7e7618b58850c7c51537f90b086c1d2c9e680
orumurin <orumurin@gmail.com>
2023-04-06 10:44:47 +0300

    create functionality for registration users

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\DTO\UserDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\LogoutRequest;
 use App\Http\Requests\Auth\RegitrationRequest;
+use App\Services\Auth\RegistrationService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        public RegistrationService $registrationService
+    )
+    {}
+
     public function registration(RegitrationRequest $request): JsonResponse
     {
-        return response()->json($request->all(), 200);
+        try {
+            $userDTO = UserDTO::from($request->all());
+
+            $userResponse = $this->registrationService->register($userDTO);
+
+            return response()->json($userResponse);
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage(), 500);
+        }
     }
 
     public function login(LoginRequest $request): JsonResponse
